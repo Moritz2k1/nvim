179b67e18c0dc8590bd0843f03307bac22cab262
Moritz2k1 <moritz.gutschi@outlook.com>
2024-12-18 00:29:26 +0100

    Added clangd lsp updated oil

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,41 +1,47 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "jedi_language_server", "ruff", "asm_lsp" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({})
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "jedi_language_server", "ruff", "asm_lsp", "clangd" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
 
-			-- LSP for Lua
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+      -- LSP for Lua
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
 
-			-- LSP for Python
-			lspconfig.jedi_language_server.setup({
-				capabilities = capabilities,
-			})
+      -- LSP for Python
+      lspconfig.jedi_language_server.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.ruff.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.ruff.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.asm_lsp.setup({
-				capabilities = capabilities,
-			})
-		end,
-	},
+      -- LSP for Assembly
+      lspconfig.asm_lsp.setup({
+        capabilities = capabilities,
+      })
+
+      -- LSP for C/C++
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+      })
+    end,
+  },
 }
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,14 +1,17 @@
 return {
-  { --Oil
-    "stevearc/oil.nvim",
-    opts = {},
-    -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+	{ --Oil
+		"stevearc/oil.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 
-    config = function()
-      require("oil").setup({
-        default_file_explorer = true,
-      })
-    end,
-  },
+		config = function()
+			require("oil").setup({
+				default_file_explorer = true,
+        keymaps = {
+          ["q"] = "actions.close", -- Close oil
+        }
+			})
+		end,
+	},
 }
