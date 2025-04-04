1459ab7fb03da614e422010b0d5857521c4e2858
Moritz2k1 <moritz.gutschi@outlook.com>
2025-04-04 19:28:25 +0200

    Remove deprecated messages

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -10,3 +10,6 @@ vim.g.maplocalleader = "\\"
 -- Enable line/relative numbers by default
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- Disable deprecated messages
+vim.deprecate = function() end
