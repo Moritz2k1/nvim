f3f93866b37314cd6d26c56b0fc221d83651896a
Moritz2k1 <moritz.gutschi@outlook.com>
2025-06-03 20:09:20 +0200

    Fix noice.nvim
    Should not give an error when cloning the repo

--- lua/plugins/interface.lua
+++ lua/plugins/interface.lua
@@ -40,19 +40,11 @@ return {
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
-		opts = {
-			-- add any options here
-		},
 		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
 			"rcarriga/nvim-notify",
 		},
-
-		require("noice").setup({
+		opts = {
 			lsp = {
 				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 				override = {
@@ -66,7 +58,7 @@ return {
 				long_message_to_split = true, -- long messages will be sent to a split
 				lsp_doc_border = false, -- add a border to hover docs and signature help
 			},
-		}),
+		},
 	},
 	-- Notify
 	{
