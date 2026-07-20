b96260aafacfd71f31fbe22f5069ef50a9d872ea
Moritz2k1 <moritz.gutschi@outlook.com>
2026-07-20 13:23:27 +0200

    Open nvim-tree when launching a file automatically

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -7,6 +7,12 @@ map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 map("", "<ScrollWheelLeft>", "<Nop>")
 map("", "<ScrollWheelRight>", "<Nop>")
 
+-- Move splits
+map("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to left window" })
+map("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to right window" })
+map("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to lower window" })
+map("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to upper window" })
+
 -- Telescope
 local telescope = require("telescope.builtin")
 map("n", "<leader>ff", telescope.find_files, { desc = "Find files" })
@@ -17,3 +23,6 @@ map("n", "<leader>fk", telescope.keymaps, { desc = "Find keymaps" })
 
 -- Lazygit
 map("n", "<leader>lg", "<cmd>LazyGit<CR>", { desc = "LazyGit" })
+
+-- Nvim-Tree
+map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle Nvim-Tree" })
